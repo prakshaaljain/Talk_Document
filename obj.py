086f8cf8c216e6732db511f26399f93193408ffc
Prakshaal Jain <prakshaal@gmail.com>
2024-05-21 19:30:17 +0530

    Object Creation

---
+++ obj.py
@@ -0,0 +1,4 @@
+
+obj = TalkDocument(HF_API_TOKEN="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",
+                   data_source_path=['C:/Users/asus/Documents/Art_Int.txt', 'C:/Users/asus/Documents/supercomputers.txt','C:/Users/asus/Downloads/recruitment_policy.txt'])
+obj.create_db_document()
